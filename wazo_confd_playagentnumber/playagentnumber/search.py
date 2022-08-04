c9d576a3d24e6a99d895a5811251703e49aa2f03
mmojtabakarimi <mmojtabakarimi@yahoo.com>
2022-08-04 19:47:23 +0430

    Add service name

--- wazo_confd_playagentnumber/playagentnumber/search.py
+++ wazo_confd_playagentnumber/playagentnumber/search.py
@@ -4,11 +4,11 @@ from xivo_dao.resources.utils.search import SearchSystem
 from .model import PlayagentnumberModel
 
 playagentnumber_config = SearchConfig(
-    table=PlayagentnumbertModel,
+    table=PlayagentnumberModel,
     columns={
-        'id': PlayagentnumbertModel.id,
-        'exten': PlayagentnumbertModel.exten,
-        'blocked_num': PlayagentnumbertModel.blocked_num,
+        'id': PlayagentnumberModel.id,
+        'exten': PlayagentnumberModel.exten,
+        'blocked_num': PlayagentnumberModel.blocked_num,
     },
     default_sort='id',
 )
